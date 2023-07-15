0b2e52f51b7fed25bc17bb4de8c4a34db7b08b1b
apl09 <adrianpastorlopez09@gmail.com>
2023-07-15 12:41:24 +0200

    crud de comments hecho

--- routes/comments.js
+++ routes/comments.js
@@ -4,5 +4,8 @@ const CommentController = require("../controllers/CommentController");
 const { authentication } = require("../middlewares/authentication");
 
 router.post("/create", authentication, CommentController.create);
+router.put('/id/:_id', authentication, CommentController.update)
+router.get('/id/:_id',authentication, CommentController.getById)
+router.delete('/id/:_id',authentication, CommentController.delete)
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -8,7 +8,6 @@ const { authentication } = require("../middlewares/authentication");
 router.post('/create', authentication, PostController.create)
 router.put('/id/:_id', authentication, PostController.update)
 router.get('/id/:_id',PostController.getById)
-router.get('/id/:_id',PostController.getById)
 router.get('/name/:name',PostController.getPostsByName)
 router.get('/id/:_id',PostController. getPostUserCommentById)
 router.delete('/id/:_id',authentication, PostController.delete)
